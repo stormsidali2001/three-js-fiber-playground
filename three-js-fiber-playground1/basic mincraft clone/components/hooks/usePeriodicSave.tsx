462799110b1d142d45c2326a31d158bd8dc1450f
Aghiles MEDANE <77809263+aghiles-medane@users.noreply.github.com>
2023-01-01 15:37:12 -0800

    Create usePeriodicSave.tsx

---
+++ three-js-fiber-playground1/basic mincraft clone/components/hooks/usePeriodicSave.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react"
+import useStore from "./useStore"
+
+const usePeriodicSave = (duration:number = 5000) => {
+     const [saveWorld,loadWorld] = useStore(state=>[state.saveWorld,state.loadWorld])
+ useEffect(()=>{
+    loadWorld()
+    const intervalId = setInterval(()=>{
+        saveWorld()
+    },duration)
+
+
+    return ()=>{
+        clearInterval(intervalId)
+    }
+ },[])
+}
+
+export default usePeriodicSave
